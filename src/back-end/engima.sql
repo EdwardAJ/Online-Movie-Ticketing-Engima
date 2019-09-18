463b8b1e1056925487a420ef3d2247f923fdc889
13517112 <13517112@std.stei.itb.ac.id>
2019-09-18 18:00:46 +0700

    Perubahan Database

--- src/back-end/engima.sql
+++ src/back-end/engima.sql
@@ -23,9 +23,9 @@ DROP TABLE IF EXISTS `genre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `genre` (
-  `idgenre` int(10) unsigned NOT NULL,
-  `nama_genre` varchar(20) NOT NULL,
-  PRIMARY KEY (`idgenre`)
+  `id_genre` char(5) NOT NULL,
+  `genre_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`id_genre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,32 +39,6 @@ LOCK TABLES `genre` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `genremovie`
---
-
-DROP TABLE IF EXISTS `genremovie`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `genremovie` (
-  `idmovie` varchar(7) NOT NULL,
-  `idgenre` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idmovie`,`idgenre`),
-  KEY `fk_genremovie_1_idx` (`idgenre`),
-  CONSTRAINT `fk_genremovie_1` FOREIGN KEY (`idgenre`) REFERENCES `genre` (`idgenre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_genremovie_2` FOREIGN KEY (`idmovie`) REFERENCES `movie` (`idmovie`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `genremovie`
---
-
-LOCK TABLES `genremovie` WRITE;
-/*!40000 ALTER TABLE `genremovie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `genremovie` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `movie`
 --
 
@@ -72,13 +46,13 @@ DROP TABLE IF EXISTS `movie`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `movie` (
-  `idmovie` varchar(7) NOT NULL,
+  `id_movie` char(7) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `runtime` int(10) unsigned NOT NULL,
-  `tanggal_rilis` date NOT NULL,
+  `tanggal_rilis` datetime NOT NULL,
   `sinopsis` longtext NOT NULL,
   `poster` longblob NOT NULL,
-  PRIMARY KEY (`idmovie`)
+  PRIMARY KEY (`id_movie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -92,6 +66,32 @@ LOCK TABLES `movie` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `moviegenre`
+--
+
+DROP TABLE IF EXISTS `moviegenre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moviegenre` (
+  `id_movie` char(7) NOT NULL,
+  `id_genre` char(5) NOT NULL,
+  PRIMARY KEY (`id_movie`,`id_genre`),
+  KEY `fk_moviegenre_1_idx` (`id_genre`),
+  CONSTRAINT `fk_moviegenre_1` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_moviegenre_2` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moviegenre`
+--
+
+LOCK TABLES `moviegenre` WRITE;
+/*!40000 ALTER TABLE `moviegenre` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moviegenre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `moviereview`
 --
 
@@ -99,15 +99,15 @@ DROP TABLE IF EXISTS `moviereview`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `moviereview` (
-  `idreview` int(10) unsigned NOT NULL,
-  `iduser` varchar(20) NOT NULL,
-  `idmovie` varchar(7) NOT NULL,
-  PRIMARY KEY (`idreview`,`iduser`,`idmovie`),
-  KEY `fk_moviereview_2_idx` (`iduser`),
-  KEY `fk_moviereview_3_idx` (`idmovie`),
-  CONSTRAINT `fk_moviereview_1` FOREIGN KEY (`idreview`) REFERENCES `review` (`idreview`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_moviereview_2` FOREIGN KEY (`iduser`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_moviereview_3` FOREIGN KEY (`idmovie`) REFERENCES `movie` (`idmovie`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_review` char(7) NOT NULL,
+  `id_movie` char(7) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_review`,`id_movie`,`username`),
+  KEY `fk_moviereview_1_idx` (`id_movie`),
+  KEY `fk_moviereview_3_idx` (`username`),
+  CONSTRAINT `fk_moviereview_1` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_moviereview_2` FOREIGN KEY (`id_review`) REFERENCES `review` (`id_review`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_moviereview_3` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -128,10 +128,10 @@ DROP TABLE IF EXISTS `review`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `review` (
-  `idreview` int(10) unsigned NOT NULL,
-  `desc` longtext NOT NULL,
-  `rating` float unsigned NOT NULL,
-  PRIMARY KEY (`idreview`)
+  `id_review` char(7) NOT NULL,
+  `description` longtext NOT NULL,
+  `rating` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_review`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -152,12 +152,12 @@ DROP TABLE IF EXISTS `schedule`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `schedule` (
-  `idschedule` char(15) NOT NULL,
+  `id_schedule` char(15) NOT NULL,
   `jadwal` datetime NOT NULL,
-  `idmovie` varchar(7) NOT NULL,
-  PRIMARY KEY (`idschedule`),
-  KEY `fk_schedule_1_idx` (`idmovie`),
-  CONSTRAINT `fk_schedule_1` FOREIGN KEY (`idmovie`) REFERENCES `movie` (`idmovie`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_movie` char(7) NOT NULL,
+  PRIMARY KEY (`id_schedule`),
+  KEY `fk_schedule_1_idx` (`id_movie`),
+  CONSTRAINT `fk_schedule_1` FOREIGN KEY (`id_movie`) REFERENCES `movie` (`id_movie`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -178,13 +178,13 @@ DROP TABLE IF EXISTS `seat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `seat` (
-  `idseat` varchar(3) NOT NULL,
-  `status` int(11) NOT NULL,
-  `idschedule` char(15) NOT NULL,
-  `harga` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idseat`,`idschedule`),
-  KEY `fk_seat_1_idx` (`idschedule`),
-  CONSTRAINT `fk_seat_1` FOREIGN KEY (`idschedule`) REFERENCES `schedule` (`idschedule`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_seat` int(10) unsigned NOT NULL,
+  `status` int(10) unsigned NOT NULL,
+  `id_schedule` char(15) NOT NULL,
+  `harga` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_seat`),
+  KEY `fk_seat_1_idx` (`id_schedule`),
+  CONSTRAINT `fk_seat_1` FOREIGN KEY (`id_schedule`) REFERENCES `schedule` (`id_schedule`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -205,16 +205,17 @@ DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaction` (
-  `idtransaction` int(10) unsigned NOT NULL,
-  `iduser` varchar(20) NOT NULL,
-  `idseat` varchar(3) NOT NULL,
-  `idschedule` char(15) NOT NULL,
-  PRIMARY KEY (`idtransaction`),
-  KEY `fk_transaction_1_idx` (`iduser`),
-  KEY `fk_transaction_3_idx` (`idschedule`),
-  CONSTRAINT `fk_transaction_1` FOREIGN KEY (`iduser`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_2` FOREIGN KEY (`iduser`) REFERENCES `seat` (`idseat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_transaction_3` FOREIGN KEY (`idschedule`) REFERENCES `schedule` (`idschedule`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `id_transaction` char(15) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `id_seat` int(10) unsigned NOT NULL,
+  `id_schedule` char(15) NOT NULL,
+  PRIMARY KEY (`id_transaction`),
+  KEY `fk_transaction_1_idx` (`username`),
+  KEY `fk_transaction_2_idx` (`id_seat`),
+  KEY `fk_transaction_3_idx` (`id_schedule`),
+  CONSTRAINT `fk_transaction_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transaction_2` FOREIGN KEY (`id_seat`) REFERENCES `seat` (`id_seat`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_transaction_3` FOREIGN KEY (`id_schedule`) REFERENCES `schedule` (`id_schedule`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -236,13 +237,15 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `username` varchar(20) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `no_hp` varchar(12) NOT NULL,
-  `picture_profile` longblob NOT NULL,
+  `email` varchar(13) NOT NULL,
+  `no_telp` varchar(50) NOT NULL,
+  `pictureprofile` longblob NOT NULL,
   `password` varchar(100) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `token_expdate` datetime NOT NULL,
   PRIMARY KEY (`username`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `no_hp_UNIQUE` (`no_hp`)
+  UNIQUE KEY `email_UNIQUE` (`no_telp`),
+  UNIQUE KEY `no_telp_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -264,4 +267,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-09-16 14:39:08
+-- Dump completed on 2019-09-18 17:57:31
