fe457bc649b8e21dd863ea6252a91292b30421e9
EdwardAJ <13517115@std.stei.itb.ac.id>
2019-09-22 16:30:00 +0700

    fix filename display in browse input box

--- src/front-end/js/register.js
+++ src/front-end/js/register.js
@@ -13,27 +13,14 @@ document.getElementById('registerButton').addEventListener('click', function () 
 })
 
 document.getElementById('browseButton').addEventListener('click', function () {
-    uploadFile();
+    document.getElementById('pic').click();
 })
 
-function uploadFile () {
-    // Click input type = "text"
-    document.getElementById('pic').click();
-    var path = document.getElementById('pic').value;
-    // Replace "fakepath" from path.
+document.getElementById('pic').onchange = function () {
+    var path = this.value;
     var filename = path.replace(/^.*\\/, "");
-    // Set text box beside browse button's value to filename
-    refreshFileUpload(filename); 
+    // Show the value of filename in input text box
     document.getElementById('file-name').value = filename;
-}
-
-function refreshFileUpload (filename) {
-    // var req = new XMLHttpRequest();
-    // req.open("POST", "address_for_div_content", true);
-    // req.onreadystatechange = function () {
-    //     if (req.readyState != 4 || req.status != 200) return;
-    //     document.getElementById('file-name').value = filename;
-    // }
 }
 
 // When register button is clicked, this function is called first.
