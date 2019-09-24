c87082e66af1f48e83434be5ba9424bd5768bc3d
Irena Irmalasari <irenairmalasari@Irenas-MacBook-Pro.local>
2019-09-24 12:51:15 +0700

    add event listener to seat, can show price

--- src/front-end/script/script.js
+++ src/front-end/script/script.js
@@ -1,5 +1,7 @@
 var button = document.getElementById("button-buy");
 var modal = document.getElementById("modal");
+var seat = document.querySelectorAll("button.seat");
+var not_booked = document.getElementById("booking-message").children;
 
 button.onclick = function(){
     modal.style.display= "block";
@@ -8,3 +10,11 @@ button.onclick = function(){
 function seatClicked(id){
     document.getElementById("movie-selected-seat").innerHTML = "Seat #" + id;
 }
+
+for (i=0; i<seat.length; i++){
+    seat[i].addEventListener('click', function() {
+        not_booked[1].style.display = "none";
+        not_booked[2].style.display = "block";
+    })
+}
+
